7e37a9c872551d4b3fb8ce3c55ff8e25dee0d060
Dimple Kuriakose <dimple.kuriakose@canonical.com>
2023-06-13 20:29:44 +0530

    Removed intersphinx mappings and their references temporarily to enable PR autobuilds.

--- conf.py
+++ conf.py
@@ -24,15 +24,6 @@ extensions = [
 
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', '.sphinx', 'readme.rst']
 
-intersphinx_mapping = {
-    'aws': ('https://canonical-aws.readthedocs-hosted.com/en/latest/', None),
-    'azure': ('https://canonical-azure.readthedocs-hosted.com/en/latest/', None),
-    'google': ('https://canonical-gcp.readthedocs-hosted.com/en/latest/', None),
-    'ibm': ('https://canonical-ibm.readthedocs-hosted.com/en/latest/', None),
-    'oracle': ('https://canonical-oracle.readthedocs-hosted.com/en/latest/', None),
-    'oci': ('https://canonical-oci.readthedocs-hosted.com/en/latest/', None)
-}
-
 #  -- Projects that will share this configuration file -----------------------------
 multiproject_projects = {
     "aws": {},
