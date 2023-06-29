4c7382b401b1463afd6d08ecabf40a2916975e89
Dimple Kuriakose <dimple.kuriakose@canonical.com>
2023-06-29 16:18:03 +0530

    Trial 4

--- conf.py
+++ conf.py
@@ -21,18 +21,6 @@ ogp_image = "https://assets.ubuntu.com/v1/6c10be67-UbuntuCloud.jpg"
 # Update with the favicon for your product
 html_favicon = ".sphinx/_static/favicon.png"
 
-html_context = {
-    # Change to the discourse instance you want to be able to link to
-    # (use an empty value if you don't want to link)
-    "discourse": "https://discourse.ubuntu.com",
-    # Change to the GitHub info for your project
-    "github_url": "https://github.com/canonical/ubuntu-cloud-docs",
-    # Change to the branch for this version of the documentation
-    "github_version": "main",
-    # Change to an empty value if your GitHub repo doesn't have issues enabled
-    "github_issues": "enabled"
-}
-
 # Used for related links - no need to change
 if 'discourse' in html_context:
     html_context['discourse_prefix'] = html_context['discourse'] + "/t/"
--- oci/conf.py
+++ oci/conf.py
@@ -1,2 +1,16 @@
 # Configuration file for the Sphinx documentation builder.
 project = 'Ubuntu on OCI Registries'
+
+html_context = {
+    # Change to the discourse instance you want to be able to link to
+    # (use an empty value if you don't want to link)
+    "discourse": "https://discourse.ubuntu.com",
+    # Change to the GitHub info for your project
+    "github_url": "https://github.com/canonical/ubuntu-cloud-docs",
+    # Change to the branch for this version of the documentation
+    "github_version": "main",
+     # Change to the folder that contains the documentation (usually "/" or "/docs/")
+    "github_folder": "/oci/",
+    # Change to an empty value if your GitHub repo doesn't have issues enabled
+    "github_issues": "enabled"
+}
