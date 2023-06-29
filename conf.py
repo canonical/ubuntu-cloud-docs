b12be00245dcdc352de65fd3db8833b5d575b9dc
Dimple Kuriakose <dimple.kuriakose@canonical.com>
2023-06-29 23:05:43 +0530

    Trial 6

--- conf.py
+++ conf.py
@@ -21,6 +21,23 @@ ogp_image = "https://assets.ubuntu.com/v1/6c10be67-UbuntuCloud.jpg"
 # Update with the favicon for your product
 html_favicon = ".sphinx/_static/favicon.png"
 
+html_context = {
+    # Change to the discourse instance you want to be able to link to
+    # (use an empty value if you don't want to link)
+    "discourse": "https://discourse.ubuntu.com",
+    # Change to the GitHub info for your project
+    "github_url": "https://github.com/canonical/ubuntu-cloud-docs",
+    # Change to the branch for this version of the documentation
+    "github_version": "main",
+    # Change to the folder that contains the documentation (usually "/" or "/docs/")
+    "github_folder": "/",
+    # Change to an empty value if your GitHub repo doesn't have issues enabled
+    "github_issues": "enabled"
+}
+
+# Used for related links - no need to change
+if 'discourse' in html_context:
+    html_context['discourse_prefix'] = html_context['discourse'] + "/t/"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -156,6 +173,10 @@ html_css_files = [
     'custom.css',
     'github_issue_links.css',
 ]
+
+html_js_files = []
+if "github_issues" in html_context and html_context["github_issues"]:
+    html_js_files.append('github_issue_links.js')
 
 
 
