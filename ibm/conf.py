fe3bca265a74bd4dc50bdda2efa1d27a28b08537
Dimple Kuriakose <dimple.kuriakose@canonical.com>
2023-06-30 15:01:04 +0530

    Trial 9

--- ibm/conf.py
+++ ibm/conf.py
@@ -3,7 +3,11 @@ project = 'Ubuntu on IBM'
 
 html_context = {
     # Change to the GitHub info for your project
-    "github_url": "https://github.com/canonical/ubuntu-cloud-docs",   
+    "github_url": "https://github.com/canonical/ubuntu-cloud-docs",
+    # Change to the branch for this version of the documentation
+    "github_version": "main",
     # Change to the folder that contains the documentation (usually "/" or "/docs/")
-    "github_folder": "/ibm/"
+    "github_folder": "/ibm/",
+    # Change to an empty value if your GitHub repo doesn't have issues enabled
+    "github_issues": "enabled"
 }
--- oracle/conf.py
+++ oracle/conf.py
@@ -9,12 +9,3 @@ html_context = {
     # Change to the folder that contains the documentation (usually "/" or "/docs/")
     "github_folder": "/oracle/"
 }
-
-
-# Used for related links - no need to change
-if 'discourse' in html_context:
-    html_context['discourse_prefix'] = html_context['discourse'] + "/t/"
-
-html_js_files = []
-if "github_issues" in html_context and html_context["github_issues"]:
-    html_js_files.append('github_issue_links.js')
