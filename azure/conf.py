d29d40a155172b9803e0fee07e4cfc3e1c986bbe
Dimple Kuriakose <dimple.kuriakose@canonical.com>
2023-06-30 14:47:12 +0530

    Trial 8

--- azure/conf.py
+++ azure/conf.py
@@ -1,2 +1,12 @@
 # Configuration file for the Sphinx documentation builder.
 project = 'Ubuntu on Azure'
+
+
+html_context = {
+    # Change to the GitHub info for your project
+    "github_url": "https://github.com/canonical/ubuntu-cloud-docs/",
+    # Change to the branch for this version of the documentation
+    "github_version": "main",
+    # Change to the folder that contains the documentation (usually "/" or "/docs/")
+    "github_folder": "/azure/"
+}
--- ibm/conf.py
+++ ibm/conf.py
@@ -1,2 +1,9 @@
 # Configuration file for the Sphinx documentation builder.
 project = 'Ubuntu on IBM'
+
+html_context = {
+    # Change to the GitHub info for your project
+    "github_url": "https://github.com/canonical/ubuntu-cloud-docs",   
+    # Change to the folder that contains the documentation (usually "/" or "/docs/")
+    "github_folder": "/ibm/"
+}
