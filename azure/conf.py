da5ba9be449081baea21da9a863965226826bb30
Dimple Kuriakose <dimple.kuriakose@canonical.com>
2023-07-03 16:58:23 +0530

    Trial 19

--- azure/conf.py
+++ azure/conf.py
@@ -1,17 +1,2 @@
 # Configuration file for the Sphinx documentation builder.
 project = 'Ubuntu on Azure'
-
-
-html_context = {    
-    
-}
-
-html_static_path = ['.sphinx/_static']
-html_css_files = [
-    'custom.css',
-    'github_issue_links.css',
-]
-
-html_js_files = []
-html_js_files.append('github_issue_links.js')
-
--- conf.py
+++ conf.py
@@ -21,6 +21,26 @@ ogp_image = "https://assets.ubuntu.com/v1/6c10be67-UbuntuCloud.jpg"
 # Update with the favicon for your product
 html_favicon = ".sphinx/_static/favicon.png"
 
+html_context = {
+    # Change to the discourse instance you want to be able to link to
+    # (use an empty value if you don't want to link)
+    "discourse": "https://discourse.ubuntu.com",
+    # Change to the GitHub info for your project
+    "github_url": "https://github.com/canonical/ubuntu-cloud-docs",
+    # Change to the branch for this version of the documentation
+    "github_version": "main",
+    # Change to the folder that contains the documentation (usually "/" or "/docs/")
+    "github_folder": "/"+ $PROJECT + "/",
+    # Change to an empty value if your GitHub repo doesn't have issues enabled
+    "github_issues": "enabled",
+    # Change to the folder that contains the documentation (usually "/" or "/docs/")
+    "conf_py_path": "/$PROJECT/"
+}
+
+# Used for related links - no need to change
+if 'discourse' in html_context:
+    html_context['discourse_prefix'] = html_context['discourse'] + "/t/"
+
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
@@ -149,6 +169,18 @@ html_theme_options = {
         "color-version-popup": "#F29879"
     },
 }
+
+html_static_path = ['.sphinx/_static']
+html_css_files = [
+    'custom.css',
+    'github_issue_links.css',
+]
+
+html_js_files = []
+if "github_issues" in html_context and html_context["github_issues"]:
+    html_js_files.append('github_issue_links.js')
+
+
 
 # Set up redirects (https://documatt.gitlab.io/sphinx-reredirects/usage.html)
 # For example: "explanation/old-name.html": "../how-to/prettify.html",
