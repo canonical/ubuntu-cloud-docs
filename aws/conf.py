5bf7db481e25b803d174c51287c6642fa474ca32
Dimple Kuriakose <dimple.kuriakose@canonical.com>
2023-07-03 13:29:49 +0530

    Trial 17

--- aws/conf.py
+++ aws/conf.py
@@ -1,9 +1,23 @@
 # Configuration file for the Sphinx documentation builder.
 project = 'Ubuntu on AWS'
 
+html_context = {
+    # Change to the GitHub info for your project
+    "github_url": "https://github.com/canonical/ubuntu-cloud-docs",
+    # Change to the branch for this version of the documentation
+    "github_version": "main",
+     # Change to the folder that contains the documentation (usually "/" or "/docs/")
+    "github_folder": "/aws/",
+    # Change to an empty value if your GitHub repo doesn't have issues enabled
+    "github_issues": "enabled"
+}
 
 html_static_path = ['.sphinx/_static']
 html_css_files = [
     'custom.css',
     'github_issue_links.css',
 ]
+
+html_js_files = []
+if "github_issues" in html_context and html_context["github_issues"]:
+    html_js_files.append('github_issue_links.js')
