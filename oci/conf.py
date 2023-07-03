f0d355d28d7acb4c47425b2bfc3b4c7ce429cbe4
Dimple Kuriakose <dimple.kuriakose@canonical.com>
2023-07-03 12:49:59 +0530

    Trial 14

--- oci/conf.py
+++ oci/conf.py
@@ -1,10 +1,7 @@
 # Configuration file for the Sphinx documentation builder.
 project = 'Ubuntu on OCI Registries'
 
-html_context = {
-    # Change to the discourse instance you want to be able to link to
-    # (use an empty value if you don't want to link)
-    "discourse": "https://discourse.ubuntu.com",
+html_context = {    
     # Change to the GitHub info for your project
     "github_url": "https://github.com/canonical/ubuntu-cloud-docs",
     # Change to the branch for this version of the documentation
@@ -12,5 +9,7 @@ html_context = {
     # Change to the folder that contains the documentation (usually "/" or "/docs/")
     "github_folder": "/oci/",
     # Change to an empty value if your GitHub repo doesn't have issues enabled
-    "github_issues": "enabled"
+    "github_issues": "enabled",
+    # Change to the folder that contains the documentation (usually "/" or "/docs/")
+    "conf_py_path": "/oci/"
 }
