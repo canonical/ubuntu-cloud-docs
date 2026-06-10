c9f9f84c341501dbe3250db60ada5d78b65f7bdd
Robby Pocase <robby.pocase@canonical.com>
2026-06-10 10:28:33 -0500

    chore(vmware): bootstrap structure
    this is a minimal commit for bootstrapping the vmware cloud

---
+++ docs/vmware/conf.py
@@ -0,0 +1,65 @@
+# Configuration file for the Sphinx documentation builder.
+project = 'Ubuntu on VMware'
+
+# The title you want to display for the documentation in the sidebar.
+# You might want to include a version number here.
+# To not display any title, set this option to an empty string.
+html_title = project + ' documentation'
+
+# Some settings in the html_context dictionary have to be repeated
+# in the sub projects for correct functioning (don't remove them)
+html_context = {
+
+    # Change to the link to the website of your product (without "https://")
+    # For example: "ubuntu.com/lxd" or "microcloud.is"
+    # If there is no product website, edit the header template to remove the
+    # link (see the readme for instructions).
+    'product_page': 'ubuntu.com/vmware',
+
+    # Add your product tag (the orange part of your logo, will be used in the
+    # header) to ".sphinx/_static" and change the path here (start with "_static")
+    # (default is the circle of friends)
+    'product_tag': '_static/tag.png',
+
+    # Change to the discourse instance you want to be able to link to
+    # using the :discourse: metadata at the top of a file
+    # (use an empty value if you don't want to link)
+    'discourse': 'https://discourse.ubuntu.com/c/project/public-cloud/',
+
+    # Change to the Mattermost channel you want to link to
+    # (use an empty value if you don't want to link)
+    'mattermost': 'https://chat.canonical.com/canonical/channels/public-cloud',
+
+    # Change to the Matrix channel you want to link to
+    # (use an empty value if you don't want to link)
+    'matrix': 'https://matrix.to/#/#ubuntu-cloud:ubuntu.com',
+
+    # Change to the GitHub URL for your project
+    'github_url': 'https://github.com/canonical/ubuntu-cloud-docs',
+
+    # Change to the branch for this version of the documentation
+    'repo_default_branch': 'main',
+
+    # Change to the folder that contains the documentation
+    # (usually "/" or "/docs/")
+    'repo_folder': '/docs/vmware/',
+
+    # Change to an empty value if your GitHub repo doesn't have issues enabled.
+    # This will disable the feedback button and the issue link in the footer.
+    'github_issues': 'enabled',
+
+    # Change to the folder that contains the documentation
+    # (usually "/" or "/docs/")
+    "conf_py_path": '/vmware/',
+
+    # Controls the existence of Previous / Next buttons at the bottom of pages
+    # Valid options: none, prev, next, both
+    'sequential_nav': "both"
+}
+
+
+############################################################
+### Sitemap configuration
+############################################################
+
+html_baseurl = 'https://documentation.ubuntu.com/vmware/'
---
+++ docs/vmware/vmware-how-to/contribute-to-these-docs.rst
@@ -0,0 +1,47 @@
+.. meta::
+   :description: Learn how to contribute to Ubuntu on VMware documentation, including editing, building, and submitting pull requests.
+
+
+Contribute to these docs
+========================
+
+.. include:: ../../reuse/contribute-to-docs.txt
+   :start-after: Start: How to contribute
+   :end-before: End: How to contribute
+
+.. include:: ../../reuse/contribute-to-docs.txt
+   :start-after: Start: Build and serve the docs (part)
+   :end-before: End: Build and serve the docs (part)
+
+.. code::
+
+	PROJECT=vmware make run
+
+Setting the `PROJECT` parameter to ``vmware`` ensures that the documentation set for `Ubuntu on VMware` gets built. This parameter is needed to distinguish between the different documentation sets present in the repository.
+
+.. include:: ../../reuse/contribute-to-docs.txt
+   :start-after: Start: How to contribute (continued)
+   :end-before: End: How to contribute (continued)
+
+Perform checks and submit a PR
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+Before opening a PR, run the following checks and also ensure that the documentation builds without any warnings (warnings are treated as errors in the publishing process):
+
+.. code::
+
+	PROJECT=vmware make spelling
+	PROJECT=vmware make linkcheck
+	PROJECT=vmware make woke
+
+If you need to add new words to the allowed list of words, include them in ``.custom_wordlist.txt``.
+
+Once all the edits are done, commit the changes and push it to your fork. From the GitHub GUI of your fork, select the commit and open a PR for it.
+
+.. include:: ../../reuse/contribute-to-docs.txt
+   :start-after: Start: Maintain content
+   :end-before: End: Maintain content
+
+.. include:: ../../reuse/contribute-to-docs.txt
+   :start-after: Start: How to contribute links
+   :end-before: End: How to contribute links
